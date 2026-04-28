8438e2578763b04a5f599e9afb99b0e5fbcf1c8c
Chris Brenton <hiring@rubyridge.org>
2026-04-28 15:32:34 -0400

    Add /validate endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,30 @@
+from flask import Flask, request
+import zulip
+
 from problem_runner import ProblemRunner
 import solutions.two_sum as two_sum_problem
+
+app = Flask(__name__)
+
+
+# Returns 200 if config is good, otherwise 300
+@app.route("/validate", methods=["POST"])
+def validate():
+    req_json = request.get_json()
+    email = req_json.get("email")
+    key = req_json.get("key")
+    url = req_json.get("url")
+    try:
+        client = zulip.Client(email=email, api_key=key, site=url)
+        result = client.get_profile()
+        if result["result"] != "success":
+            return result["msg"], 400
+    except zulip.ZulipError as e:
+        return e.args[0], 400
+    except Exception as e:
+        return "non-zulip error", 400
+
+    return "gucci", 200
 
 
 if __name__ == "__main__":
