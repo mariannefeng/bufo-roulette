5107b17d85d3eb1a0f7e2f783040cb3a13d83d7a
Marianne Feng <feng.marianne@gmail.com>
2026-04-28 15:30:14 -0400

    frontend stubs, things are happening

---
+++ frontend/src/components/Game.tsx
@@ -0,0 +1,61 @@
+import CodeEditor from '@uiw/react-textarea-code-editor';
+import bufoLock from '../assets/bufo-lock.png'
+import bufoShip from '../assets/bufo-ship.png'
+import { useState } from 'react';
+
+const API_URL = import.meta.env.VITE_API_URL || '';
+
+export default function Game() {
+    const [code, setCode] = useState("");
+    const [success, setSuccess] = useState(false);
+
+    const fuckMeUp = () => {
+        const confirmed = window.confirm("Are you ready?");
+
+        if (!confirmed) {
+            return
+        }
+
+        console.log('inside of fuck me up')
+
+        fetch(`${API_URL}/fuck-me-up`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/JSON",
+            },
+            body: JSON.stringify(code),
+        })
+            .then((response: Response) => {
+                if (!response.ok) {
+                    throw new Error("Failed validation")
+                }
+
+                return response.json()
+            }).then((resp) => {
+                console.log('json response', resp)
+                setSuccess(true);
+            })
+            .catch((error) => {
+                console.log(error);
+                setSuccess(false);
+            });
+    }
+
+    return <section>
+        <div className="flex justify-center"><img src={bufoLock} /></div>
+        <pre>{`const twoSum = (array, goal) => { `}</pre>
+        <CodeEditor
+            value={code}
+            language="ts"
+            minHeight={300}
+            onChange={(evn) => setCode(evn.target.value)}
+        />
+        <pre>{`}`}</pre>
+
+        <div className='mt-10 text-right'>
+            <button className="rounded-md bg-bufo-dark hover:bg-bufo-light hover:text-black text-white px-2 py-1 cursor-pointer" onClick={fuckMeUp}>
+                ship it <img src={bufoShip} />
+            </button>
+        </div>
+    </section>
+}
---
+++ frontend/src/components/Welcome.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import bufoFight from '../assets/bufo-fight.gif'
+
+
+export default function Welcome({ onSubmit }: { onSubmit: ({ }: { email: string, key: string, url: string }) => void }) {
+    const [email, setEmail] = useState("")
+    const [key, setKey] = useState("")
+    const [url, setURL] = useState("")
+
+    return (
+        <section>
+            <div>Welcome to bufo roulette. To get started, we'll need your:</div>
+            <div className="flex flex-col items-center mt-10 gap-3">
+                <input className="border-2 border-gray-300 rounded-md p-1" type="email" value={email} placeholder="Your Zulip email" onChange={(event) => setEmail(event.target.value)} />
+                <input className="border-2 border-gray-300 rounded-md p-1" type="text" value={key} placeholder="Your Zulip API Key" onChange={(event) => setKey(event.target.value)} />
+                <input className="border-2 border-gray-300 rounded-md p-1" type="url" value={url} placeholder="Your Zulip URL" onChange={(event) => setURL(event.target.value)} />
+            </div>
+
+            <div className="mt-5">
+                <button className="rounded-md bg-bufo-dark hover:bg-bufo-light hover:text-black text-white px-2 py-1 cursor-pointer" onClick={(event) => {
+                    event.preventDefault();
+                    onSubmit({ email, key, url });
+                }}>Let me at it!<img src={bufoFight} /></button>
+            </div>
+        </section>
+    )
+}
