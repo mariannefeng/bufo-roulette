e731390904b4501201656984e4150080bff7677e
Chris Brenton <hiring@rubyridge.org>
2026-04-29 17:41:41 -0400

    wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css'
 import Welcome from './components/Welcome';
 import Game from './components/Game';
+import { useEffect } from "react";
 
 const API_URL = import.meta.env.VITE_API_URL || '';
 
@@ -26,12 +27,37 @@ function App() {
       }).then((resp) => {
         console.log('json response', resp)
         setSessionToken(JSON.parse(resp)['session_token'])
+        console.log(sessionToken);
+        console.log({sessionToken});
         setEligibleToPlay(true)
       })
       .catch((error) => {
         console.log(error);
         alert("Oh no! Looks like your creds are trash")
       });
+
+      console.log("foo");
+      const session_token = sessionToken;
+      console.log(session_token);
+      const onBeforeUnload = (ev) => {
+        
+        //#############     
+        console.log({sessionToken});
+        //#############
+        
+        navigator.sendBeacon(`${API_URL}/chicken-out`, JSON.stringify({ session_token: {sessionToken} }));
+
+        ev.returnValue = "Anything you wanna put here!";
+        return "Anything here as well, doesn't matter!";
+      };
+
+      window.addEventListener("beforeunload", onBeforeUnload);
+
+      return () => {
+        window.removeEventListener("beforeunload", onBeforeUnload);
+      };
+
+
   }
 
   return (
